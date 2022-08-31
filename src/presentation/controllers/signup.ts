728b1b6b3dbd5a02c4278ead78f41b9139b9e7e8
Edson Sousa <edsoncarlos33@hotmail.com>
2022-08-30 22:12:51 -0300

    Revert "refactor: make SignUpController conforms to a protocol"
    This reverts commit 7597f2eb5ebeccb4da8466b183324bd515c37a60.

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,8 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
-import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
-export class SignUpController implements Controller {
+export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
