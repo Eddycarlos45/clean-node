ae83458aeb0216f5b792dfdb41306addc782ac21
Edson Sousa <edsoncarlos33@hotmail.com>
2022-10-11 14:29:13 -0300

    feat: add mongodb connection

---
+++ src/main/config/env.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export default {
+  mongoUrl: process.env.MONGO_URL,
+  port: process.env.PORT || 3000
+}
